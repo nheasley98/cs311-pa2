cfa51cf90c02101380f5b328154274f4ec90b7f6
Noah Heasley <nheasley@iastate.edu>
2019-12-12 21:18:34 -0600

    Added skeleton for ImageProcessor, continuing to work on it

---
+++ src/pa2/ImageProcessor.java
@@ -0,0 +1,26 @@
+package PA2;
+
+import api.Tuple;
+import api.Picture;
+import api.ImageStitch;
+
+/**
+ * @author Mark Schmidt-Dannert and Noah Heasley
+**/
+public class ImageProcessor {
+
+	/**
+	 * Your  method must use  the  algorithm  described  earlier to  reduce  the  image  width.
+	 * And  your method must use the static methods from the
+	 * class MatrixCuts to compute the width cut and reduce the width.  Otherwise, you will receive zero credit.
+	 *
+	 * @param x
+	 * @param inputImage is the nameof image whose width will be reduced.
+	 * @return Picture with width W - param x
+	 */
+	static Picture reduceWidth(int x, String inputImage) {
+
+
+		
+	}
+}
